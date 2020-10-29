7c82098f66e2b046309ee52d7068258e774738c1
sezginozgurr <sezginzgurr@gmail.com>
2020-10-29 16:21:18 +0300

    fix

--- app/src/main/java/com/example/corotinestest/ui/fragments/CumhuriyetFragment.kt
+++ app/src/main/java/com/example/corotinestest/ui/fragments/CumhuriyetFragment.kt
@@ -1,22 +1,30 @@
 package com.example.corotinestest.ui.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.corotinestest.R
+import com.example.corotinestest.core.model.NewsCategoryTitleModel
+import com.example.corotinestest.core.model.trnews.Article
+import com.example.corotinestest.ui.adapter.BaseAdapter
+import com.example.corotinestest.ui.viewmodel.HomePageViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
-class CumhuriyetFragment : Fragment() {
+class CumhuriyetFragment : Fragment(R.layout.fragment_sabah) {
 
+    private var adapter: BaseAdapter<NewsCategoryTitleModel>? = null
+    private var adapterNews: BaseAdapter<Article>? = null
+    private var newsList: ArrayList<Article> = ArrayList()
+    val categoryList = mutableListOf<NewsCategoryTitleModel>()
+    private var navController: NavController? = null
+    private val homeViewModel by viewModel<HomePageViewModel>()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sabah, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
     }
 
 }
